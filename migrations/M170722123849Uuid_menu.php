12e2932d1948b75281911a280939085239f0b00d
Nex Otaku <nex@otaku.ru>
2017-07-22 15:45:00 +0300

    Добавил миграции.

---
+++ migrations/M170722123849Uuid_menu.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace nex_otaku\admin\migrations;
+
+use dmstr\db\mysql\FileMigration;
+
+class M170722123849Uuid_menu extends FileMigration
+{
+    # create a sql file `M170722123849Uuid_menu.sql` or adjust and uncomment the following line, do not change this class name
+    //public $file = 'custom-filename.sql';
+}
---
+++ migrations/M170722123849Uuid_menu.sql
@@ -0,0 +1,30 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               5.5.53 - MySQL Community Server (GPL)
+-- Операционная система:         Win32
+-- HeidiSQL Версия:              9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Дамп структуры для таблица menu
+CREATE TABLE IF NOT EXISTS `menu` (
+  `id` binary(16) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `parent` binary(16) DEFAULT NULL,
+  `route` varchar(256) DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  `data` text,
+  PRIMARY KEY (`id`),
+  KEY `menu_ibfk_1` (`parent`),
+  CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `menu` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
